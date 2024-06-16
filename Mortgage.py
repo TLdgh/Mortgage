8174a720a9e16f6c81ebe7d89c6912011ef035f9
TLdgh <teng.li2@mail.mcgill.ca>
2024-06-16 13:29:10 -0400

    Added variable interest rates option.

--- Mortgage.py
+++ Mortgage.py
@@ -3,31 +3,44 @@ import pandas as pd
 import plotly.express as px
 
 class Amortization:
-    def __init__(self, Balance:list, Rate:float, N=25, Payment=[0],):
+    def __init__(self, Balance:list, Rate:list, N=25, Payment=[0],):
         self.Balance=Balance
         self.Rate=Rate
         self.N=N
         self.Interests=[0]
+        self.IntRate=[0]
         self.Principal=[0]
         self.Payment=Payment
     
     def CalcPayment(self, N):
-        t=0
-        Time = list(range(0, N+1, 1))
-        L=self.Balance[0]*self.Rate*(1+self.Rate)**N/((1+self.Rate)**N - 1)
+        for rate in self.Rate:
+            trunc_point=rate[0]
+            n=N-trunc_point+1
+            r=rate[1]
+            self.Balance=self.Balance[0:trunc_point]
+            self.Interests=self.Interests[0:trunc_point]
+            self.Principal=self.Principal[0:trunc_point]
+            self.Payment=self.Payment[0:trunc_point]
+            self.IntRate=self.IntRate[0:trunc_point]
+        
+            L=self.Balance[-1]*r*(1+r)**n/((1+r)**n - 1)
+        
+            t=trunc_point-1
+            while self.Balance[-1]>0:
+                self.Interests.append(self.Balance[t]*r)
+                self.Principal.append(L-self.Interests[t+1])
+                self.Balance.append(self.Balance[t]-self.Principal[t+1])
+                t+=1
+            
+            self.Payment=self.Payment+[L]*n
+            self.IntRate=self.IntRate+[r]*n
 
-        while self.Balance[-1] >=0:
-            self.Interests.append(self.Balance[t]*self.Rate)
-            self.Principal.append(L-self.Interests[t+1])
-            self.Balance.append(self.Balance[t]-self.Principal[t+1])
-            t+=1
-        self.Payment=self.Payment+[L]*t
-        self.printAmrt(t, Time)
+        self.printAmrt(t=N+1, Time= list(range(0, N+1, 1)))
         
     def CalcTime(self):
         t=0
         Time = [0]
-        
+
         if(self.Payment==[0]):
             error_message="Payment amount cannot be 0."
             raise ValueError(error_message)
@@ -35,7 +48,9 @@ class Amortization:
             self.Payment=[0]+self.Payment
         
         while self.Balance[-1] >0 and t<(len(self.Payment)-1):
-            self.Interests.append(self.Balance[t]*self.Rate)
+            r=self.determine_r(self.Rate, t+1)
+            self.IntRate.append(r)
+            self.Interests.append(self.Balance[t]*r)
             if(self.Payment[t+1]<self.Interests[t+1]):
                 error_message="Payment amount cannot be less than the interest"
                 raise ValueError(error_message)
@@ -50,15 +65,26 @@ class Amortization:
                 
             t+=1
             Time.append(t)
-        self.printAmrt(t, Time)
+        
+        self.printAmrt(t+1, Time)
     
     def printAmrt(self,t,Time):
-        data={"Time": Time[:t+1], 
-              "Payment": self.Payment[:t+1], 
-              "Interests": self.Interests[:t+1], 
-              "Principal": self.Principal[:t+1], 
-              "Balance": self.Balance[:t+1]}
+        data={"Time": Time, 
+              "IntRate": self.IntRate[:t],
+              "Payment": self.Payment[:t], 
+              "Interests": self.Interests[:t], 
+              "Principal": self.Principal[:t], 
+              "Balance": self.Balance[:t]}
         df=pd.DataFrame(data).round(3)
         self.data=df
         print(df)
+    
+    def determine_r(self, Rate, t):
+        for i in range(len(Rate) - 1):
+            start, rate = Rate[i]
+            end, temp_r = Rate[i + 1]
+            if start <= t < end:
+                return rate
+        # If t is greater than the last start value, return the last rate
+        return Rate[-1][1]
 
--- tests/Mortgage_test.py
+++ tests/Mortgage_test.py
@@ -4,29 +4,29 @@ from Mortgage import Amortization
 
 #use the default value N=25 in CalcPayment
 def test1():
-    result=Amortization(Balance=[30000], Rate=0.03/12)
+    result=Amortization(Balance=[30000], Rate=[[1,0.03/12]])
     result.CalcPayment(result.N) #use the default value
     data=result.data
     
     assert data is not None and len(data["Time"]) == 26 and round(data["Payment"][1],3)==1239.389, "Failed to generate data frame."
 
 def test2():
-    result=Amortization(Balance=[30000], Rate=0.03/12)
-    result.CalcPayment(N=48) #use the customized value
+    result=Amortization(Balance=[30000], Rate=[[1, 0.03/12]])
+    result.CalcPayment(N=48) #use the customized value of N
     data=result.data
     
     assert data is not None and len(data["Time"]) == 49 and round(data["Payment"][1],3)==664.03, "Failed to generate data frame."
 
 def test3():
     #Customized payment schedule and estimate time needed:
-    result=Amortization(Balance=[30000], Rate=0.03/12)#use the default value
+    result=Amortization(Balance=[30000], Rate=[[1, 0.03/12]])#use the default value
     
     with pytest.raises(ValueError, match= "Payment amount cannot be 0."):
         result.CalcTime()
 
 def test4():
     #Customized payment schedule and estimate time needed:
-    result=Amortization(Balance=[30000], Rate=0.03/12, Payment=[0]+[664.03]*12)
+    result=Amortization(Balance=[30000], Rate=[[1, 0.03/12]], Payment=[0]+[664.03]*12)
     result.CalcTime()
     data=result.data
     
@@ -34,15 +34,23 @@ def test4():
 
 def test5():
     #Customized payment schedule and estimate time needed:
-    result=Amortization(Balance=[30000], Rate=0.03/12, Payment=[664.03]*12)
+    result=Amortization(Balance=[30000], Rate=[[1, 0.03/12]], Payment=[664.03]*12)
     result.CalcTime()
     data=result.data
     
     assert data is not None and len(data["Time"]) == 13, "Failed to generate data frame."
 
-def test5():
+def test6():
     #Customized payment schedule and estimate time needed:
-    result=Amortization(Balance=[30000], Rate=0.03/12, Payment=[664.03]*12+[10000]+[1200]*12)
+    result=Amortization(Balance=[30000], Rate=[[1, 0.03/12]], Payment=[664.03]*12+[10000]+[1200]*12)
+    result.CalcTime()
+    data=result.data
+    
+    assert data is not None and len(data["Time"]) == 25 and round(data["Payment"][13],3)==10000.000, "Failed to generate data frame."
+
+def test7():
+    #Customized payment schedule and estimate time needed:
+    result=Amortization(Balance=[30000], Rate=[[1, 0.03/12], [13, 0.01/12]], Payment=[664.03]*12+[10000]+[1200]*12)
     result.CalcTime()
     data=result.data
     
